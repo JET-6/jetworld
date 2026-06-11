89ee78361fa801643e666525813865bac50529f0
JET <thomas.jimi@gmail.com>
2026-06-10 20:35:16 -0700

    undid globeContainer...shutdownJETWORLD to troubleshoot error

--- scripts.js
+++ scripts.js
@@ -110,8 +110,9 @@ function shutdownJETWORLD() {
 
   // Optional: fade out audio, stop animation loop, etc.
 }
-globeContainer.addEventListener('click', shutdownJETWORLD);
-
+globeContainer.addEventListener('click', () => {
+  shutdownJETWORLD();
+});
 
 /* IF WE WANT TO REDIRECT OR LEAD INTO SOMETHING ELSE
 globeContainer.addEventListener('click', () => {
